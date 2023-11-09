14d08d60c1f44dc41cd2f5ff6696466732518414
tx2107 <150204900+tx2107@users.noreply.github.com>
2023-11-09 13:45:21 -0500

    Update main_tian.py

--- main_tian.py
+++ main_tian.py
@@ -8,6 +8,7 @@ OUTPUT_NAMES_LIST = ['Months', 'Paymnt_Count', 'Paydate', 'Scheduled_Principal',
                      'Scheduled_Balance',
                      'Prepay_Speed', 'Default_Rate', 'Recovery', 'Servicing_CF', 'Earnout_CF', 'Balance', 'Principal',
                      'Default', 'Prepay', 'Interest_Amount', 'Total_CF']
+
 INPUT_NAMES_LIST = ['Valuation_Date', 'Grade', 'Issue_Date', 'Term', 'CouponRate',
                     'Invested', 'Outstanding_Balance', 'Recovery_Rate',
                     'Purchase_Premium', 'Servicing_Fee', 'Earnout_Fee',
@@ -307,12 +308,17 @@ if __name__ == "__main__":
     python3 main_tian.py --Valuation_Date xxx --Grade C2 --Default_Multiplier 2
     python3 main_tian.py --Valuation_Date xxx --Grade C1 --Default_Multiplier 2 --Issue_Date 2023-08-09
     python3 main_tian.py --Valuation_Date xxx --Grade C4 --Deafult_Multiplier 1 --Issue_Date 10/07/2023
+    python3 main_tian.py --Valuation_Date xxx --Grade C4 --Deafult_Multiplier 1 --Invested 1000
 
     fixed the date of 31st issues to match the DATE function in excel in case Issue_Date_data.day = 31
     
     xlsx file was saved at the same directory of python codes
     xlsx = pd.ExcelFile('Loan IRR.xlsx')
     the content in the Excel will need to exactly match what you send me in the email.
+    OUTPUT_NAMES_LIST = ['Months', 'Paymnt_Count', 'Paydate', 'Scheduled_Principal', 'Scheduled_Interest',
+                     'Scheduled_Balance',
+                     'Prepay_Speed', 'Default_Rate', 'Recovery', 'Servicing_CF', 'Earnout_CF', 'Balance', 'Principal',
+                     'Default', 'Prepay', 'Interest_Amount', 'Total_CF']
 
 
     '''
