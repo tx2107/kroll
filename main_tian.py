7aa5055d39808b6bc81cd0667aa76c9177cc23a8
tx2107 <150204900+tx2107@users.noreply.github.com>
2023-11-09 08:03:44 -0500

    Update main_tian.py

--- main_tian.py
+++ main_tian.py
@@ -212,6 +212,7 @@ def irr_flow_preparation(Valuation_Date: str = "12/31/2017", Grade: str = "C4", 
 
 if __name__ == "__main__":
     '''
+    I AM USING PYTHON 3.11.
     assumption: date_format is a string with format of  "%m/%d/%Y"
     fixed the date of 31st issues to match the DATE function in excel in case Issue_Date_data.day = 31
     xlsx file was saved at the same directory of python codes
